973b380664696f9e7fa913449fa9ac879b5898f7
mihir224 <mihirsaini25@gmail.com>
2023-06-06 02:19:40 +0530

    added jspdf and some more functionality

--- src/components/Download.js
+++ src/components/Download.js
@@ -3,9 +3,12 @@ import '../styles/Download.css';
 import {storage} from '../firebase';
 import {ref,listAll,getDownloadURL,getMetadata} from 'firebase/storage';
 import axios from 'axios';
+import { jsPDF } from "jspdf";
 
 function Download(){
+    const doc=new jsPDF();
     const [fileList,setFileList]=useState([]);
+    const [downloadUrlList,setDownloadUrlList]=useState([]);
     const [isLoading,setIsLoading]=useState(false);
     const fileListRef=ref(storage,'/uploads');
     useEffect(()=>{
@@ -17,6 +20,8 @@ function Download(){
                 res.items.forEach(async (item)=>{
                    const response=await getMetadata(item);
                    setFileList((prev)=>[...prev,response])
+                   const downloadUrl=await getDownloadURL(item)
+                   setDownloadUrlList((prev)=>[...prev,downloadUrl]);
                 })
                 setIsLoading(false);
             }catch(err){
@@ -24,10 +29,19 @@ function Download(){
             }
         })();
     },[])
-
-useEffect(()=>{
+    
+    useEffect(()=>{
        console.log(fileList)
     },[fileList])
+
+    const generateReport=()=>{
+        try{
+            doc.text("hello",10,10)
+            doc.save("a4.pdf")
+        }catch(err){
+            console.log(err)
+        }
+    }
 
     return isLoading?<h2 style={{opacity:'0.9',textAlign:'center'}}>Loading...</h2>:(
         <div id='download'>
@@ -35,11 +49,11 @@ useEffect(()=>{
                 <div className='col'>Uploaded File</div>
                 <div className='col'>Report</div>
             </div>
-            {fileList.map((file)=>{
+            {fileList.map((file,index)=>{
                 return(
                     <div className='grid-container' id='grid-container-row'>
-                        <div className='row'>{file?.name}</div>
-                        <div className='row'>file-report.pdf</div>
+                        <div className='row'><a id='uploaded-file-link' href={downloadUrlList[index]} target='_blank' ><span>{file?.name}</span></a></div>
+                        <div className='row'><span id='report-text' onClick={generateReport}>file-report.pdf</span></div>
                     </div>
                 )
             })}
